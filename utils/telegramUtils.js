fb9232b8d2c852d4c4705c174b5e79cb0799d375
makarenkoj <makarenkoj53@gmail.com>
2025-02-13 23:04:36 +0200

    fix message handler

--- utils/telegramUtils.js
+++ utils/telegramUtils.js
@@ -8,9 +8,9 @@ async function sendAndHandleMessages(client, channelName, userMessage, channelMe
 
     client.addEventHandler(async (update) => {
       if (update.message) {
-        const message = update.message.message;
+        const message = update.message.message || update.message;
 
-        await handleTriggerInMessage(user, message);
+        await handleTriggerInMessage(user, update, client);
         console.log('Вхідне повідомлення:', message);
       }
     });
@@ -21,22 +21,38 @@ async function sendAndHandleMessages(client, channelName, userMessage, channelMe
   }
 };
 
-async function handleTriggerInMessage(user, message) {
+async function handleTriggerInMessage(user, update, client) {
   try {
     const userDevicesTriggers = await getUserTriggers(user);
+    const message = update.message.message || update.message;
     
     for (const { device, triggers } of userDevicesTriggers) {
       for (const trigger of triggers) {
-        if (message.includes(trigger.triggerOn)) {
-          await controlDevice(device._id, true, device.accessId, device.secretKey) //add roolse to change devices
-        } else if (message.includes(trigger.triggerOff)) {
-          await controlDevice(device._id, false, device.accessId, device.secretKey)
-        };
+        let chat;
+
+        try {
+          chat = await client.getEntity(trigger.chanelName);
+        } catch (error) {
+          console.error(`Канал ${trigger.chanelName} не знайдено:`, error);
+          continue;
+        }
+
+        if (update?.message && update?.message?.peerId?.channelId?.value === chat?.id?.value) {
+          if (message?.includes(trigger.triggerOn)) {
+            console.log('Trigger ON:', message);
+            await controlDevice(device._id, true, device.accessId, device.secretKey);
+          } else if (message?.includes(trigger.triggerOff)) {
+            console.log('Trigger Off:', message);
+            await controlDevice(device._id, false, device.accessId, device.secretKey);
+          }
+        } else if (message?.includes(trigger.triggerOff)) {
+          await controlDevice(device._id, false, device.accessId, device.secretKey);
+        }
       }
     }
   } catch (error) {
-    console.error('Помилка під час надсилання повідомлень:', error);
+    console.error('Помилка під час обробки повідомлень:', error);
   }
-};
+}
 
 export{ sendAndHandleMessages };
